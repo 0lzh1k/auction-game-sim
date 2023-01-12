f71338241fd4bdcf89c8dd29a3e2a4d9da42e26f
Olzhas <olzhas.khamzin@outlook.com>
2023-01-12 13:11:00 +0500

    game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -6,3 +6,16 @@ from typing import Dict, Any, List
 import pandas as pd
 import numpy as np
 
+def run_auction_simulation(auction_type: str, num_bidders: int, num_simulations: int,
+                          valuation_dist: str, valuation_params: Dict[str, float],
+                          strategies: List[str]):
+    simulator = AuctionSimulator()
+    results = simulator.run_simulation(
+        auction_type=auction_type,
+        num_bidders=num_bidders,
+        num_simulations=num_simulations,
+        valuation_distribution=valuation_dist,
+        valuation_params=valuation_params,
+        strategies=strategies
+    )
+    return results
