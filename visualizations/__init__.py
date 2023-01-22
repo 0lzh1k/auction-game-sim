5ba92ec6a28f379884c9200336f9297b91b01868
Olzhas <olzhas.khamzin@outlook.com>
2023-01-22 13:11:00 +0500

    add to init.py

--- visualizations/__init__.py
+++ visualizations/__init__.py
@@ -1,11 +1,13 @@
 from .plots import (
     plot_bid_distribution,
     plot_revenue_comparison,
-  
+    plot_strategy_performance,
+   
 )
 
 __all__ = [
     'plot_bid_distribution',
     'plot_revenue_comparison', 
-   
+    'plot_strategy_performance',
+ 
 ]
