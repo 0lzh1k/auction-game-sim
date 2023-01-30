18b084ae6fc21d393fae42bf98a62061717922ec
Olzhas <olzhas.khamzin@outlook.com>
2023-01-30 13:11:00 +0500

    init.py

--- auction_simulator/agents.py
+++ auction_simulator/agents.py
@@ -12,3 +12,19 @@ class Agent:
         self.payoff = 0.0
         self.won = False
     
+ 
+    
+    def calculate_payoff(self, payment: float) -> float:
+        if self.won:
+            self.payoff = self.valuation - payment
+        else:
+            self.payoff = 0.0
+        return self.payoff
+    
+    def reset(self):
+        self.bid = 0.0
+        self.payoff = 0.0
+        self.won = False
+    
+    def __repr__(self):
+        return f"Agent({self.agent_id}, v={self.valuation:.2f}, bid={self.bid:.2f})"
