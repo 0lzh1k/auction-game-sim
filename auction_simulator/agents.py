6ac395c2885fd507835b202582590acf92969cc9
Olzhas <olzhas.khamzin@outlook.com>
2023-02-04 13:11:00 +0500

    init.py

--- auction_simulator/agents.py
+++ auction_simulator/agents.py
@@ -11,19 +11,7 @@ class Agent:
         self.bid = 0.0
         self.payoff = 0.0
         self.won = False
-    
-    def place_bid(self, auction_type: str, num_bidders: int, **kwargs) -> float:
-        from .strategies import BiddingStrategy
-        
-        strategy_obj = BiddingStrategy(self.strategy)
-        self.bid = strategy_obj.calculate_bid(
-            valuation=self.valuation,
-            auction_type=auction_type,
-            num_bidders=num_bidders,
-            **kwargs
-        )
-        return self.bid
-    
+
     def calculate_payoff(self, payment: float) -> float:
         if self.won:
             self.payoff = self.valuation - payment
