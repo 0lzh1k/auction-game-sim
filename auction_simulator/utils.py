427e89380f491a1d39a185c6b85123cd454a1ecd
Olzhas <olzhas.khamzin@outlook.com>
2023-02-28 13:11:00 +0500

    utils.py

--- auction_simulator/utils.py
+++ auction_simulator/utils.py
@@ -78,6 +78,3 @@ def calculate_auction_efficiency(results: List[Any]) -> Dict[str, float]:
 def format_currency(amount: float) -> str:
     return f"${amount:.2f}"
 
-
-def format_percentage(value: float) -> str:
-    return f"{value * 100:.1f}%"
